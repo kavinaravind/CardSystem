f724f8cfca8d0e9bd8a623193e77eb90560ce124
Kavin Aravind <aravindk@vt.edu>
2016-06-07 18:46:07 -0400

    document parsing

--- CoLocatedCardSystem/CollaborationWindow/DocumentModule/DocumentController.cs
+++ CoLocatedCardSystem/CollaborationWindow/DocumentModule/DocumentController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace CoLocatedCardSystem.CollaborationWindow.DocumentModule
 {
@@ -17,12 +20,29 @@ namespace CoLocatedCardSystem.CollaborationWindow.DocumentModule
         /// Initialize documents from jsonFile
         /// </summary>
         /// <param name="jsonFilePath"></param>
-        public void Init(String jsonFilePath) {
-            //TO DO initialize the document list.
+        public async void Init(String jsonFilePath) {
             list = new DocumentList();
-            string nextLine = "";
-            while (nextLine != null) {
-                list.AddDocument(nextLine);
+            StorageFolder folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            StorageFile file = await folder.GetFileAsync(jsonFilePath);
+            var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+            ulong size = stream.Size;
+
+            using (var inputStream = stream.GetInputStreamAt(0))
+            {
+                using (var dataReader = new Windows.Storage.Streams.DataReader(inputStream))
+                {
+                    uint numBytesLoaded = await dataReader.LoadAsync((uint)size);
+                    string allText = dataReader.ReadString(numBytesLoaded);
+                    string[] lines = allText.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach(string str in lines) {
+                        Debug.WriteLine("TEXT: " + str);
+                        Document doc = new Document();
+                        doc.Load(str);
+                        //list.AddDocument(doc);
+                        //Debug.WriteLine(doc.GetContent());
+                    }
+                }
             }
         }
         /// <summary>
